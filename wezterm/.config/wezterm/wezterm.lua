1a038f8bc2e92b0e0e1b950a5861128ded753d32
Alexis Michel <michel.alexis@outlook.com>
2024-10-08 21:36:36 +0200

    chore: add status action

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = wezterm.config_builder()
 
@@ -86,6 +87,12 @@ config.keys = {
     key = 'UpArrow',
     action = wezterm.action.AdjustPaneSize { 'Up', 5 },
   },
+  -- past
+  {
+    mods = 'CTRL',
+    key = 'V',
+    action = act.PasteFrom 'Clipboard',
+  },
 }
 
 for i = 0, 9 do
@@ -100,11 +107,63 @@ end
 config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
+config.status_update_interval = 1000
 
--- Always open on fullscreen
 wezterm.on('gui-startup', function()
   local tab, pane, window = wezterm.mux.spawn_window {}
+  -- Always open on fullscreen
   window:gui_window():maximize()
+end)
+
+wezterm.on("update-status", function(window, pane)
+  local basename = function(s)
+    -- Nothing a little regex can't fix
+    return string.gsub(s, '(.*[/\\])(.*)', '%2')
+  end
+
+  -- Current working directory
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    if type(cwd) == 'userdata' then
+      -- Wezterm introduced the URL object in 20240127-113634-bbcac864
+      cwd = basename(cwd.file_path)
+    else
+      -- 20230712-072601-f4abf8fd or earlier version
+      cwd = basename(cwd)
+    end
+  else
+    cwd = ''
+  end
+
+  -- Current command
+  local cmd = pane:get_foreground_process_name()
+  -- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+  cmd = cmd and basename(cmd) or ''
+
+  -- Time
+  local time = wezterm.strftime '%H:%M'
+
+  -- Left status (left of the tab line)
+  -- window:set_left_status(wezterm.format {
+  --   { Foreground = { Color = stat_color } },
+  --   { Text = '  ' },
+  --   { Text = wezterm.nerdfonts.oct_table .. '  ' .. stat },
+  --   { Text = ' |' },
+  -- })
+
+  -- Right status
+  window:set_right_status(wezterm.format {
+    -- Wezterm has a built-in nerd fonts
+    -- https://wezfurlong.org/wezterm/config/lua/wezterm/nerdfonts.html
+    { Text = wezterm.nerdfonts.md_folder .. '  ' .. cwd },
+    { Text = ' | ' },
+    { Foreground = { Color = '#ca9ee6' } },
+    { Text = wezterm.nerdfonts.fa_code .. '  ' .. cmd },
+    'ResetAttributes',
+    { Text = ' | ' },
+    { Text = wezterm.nerdfonts.md_clock .. '  ' .. time },
+    { Text = '  ' },
+  })
 end)
 
 return config
